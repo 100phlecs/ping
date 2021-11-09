2ff0d41b357ae27342c5a664916181b1ff30f396
100phlecs <hello@based.town>
2021-11-09 11:10:54 -0500

    stitch together sycamore and tauri

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,9 @@
-use iced::{executor, Application, Command, Element, Settings, Text};
+// main.rs
 
-pub fn main() -> iced::Result {
-    Hello::run(Settings::default())
-}
+use sycamore::prelude::*;
 
-struct Hello;
-
-impl Application for Hello {
-    type Executor = executor::Default;
-    type Message = ();
-    type Flags = ();
-
-    fn new(_flags: ()) -> (Hello, Command<Self::Message>) {
-        (Hello, Command::none())
-    }
-
-    fn title(&self) -> String {
-        String::from("A cool application")
-    }
-
-    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
-        Command::none()
-    }
-
-    fn view(&mut self) -> Element<Self::Message> {
-        Text::new("Hello, world!").into()
-    }
+pub fn main() {
+    sycamore::render(|| view! {
+        p { "Hello, World!" }
+    });
 }
